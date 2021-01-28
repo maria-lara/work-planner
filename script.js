08113debccaf2382563e84df6a130c11714e90ec
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-01-27 19:01:35 -0800

    update css

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var timeNow = parseInt(moment().format('HH'));
 var hours = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17",];
 
 // Add today's date to the Current Day display on html //
-$("#currentday").append(today);
+$("#currentDay").append(today);
 
 // Create loop to find past, current, and future time for styling //
 currentTime();
@@ -14,13 +14,13 @@ function currentTime() {
 
     for(var i = 0; i < hours.length; i++){
         if (timeNow === parseInt(hours[i])){
-            $("#" + hours[i]).addClass("red");
-        }
-        else if (timeNow > parseInt(hours[i])){
             $("#" + hours[i]).addClass("green");
         }
-        else if(timeNow < parseInt(hours[i])){
+        else if (timeNow > parseInt(hours[i])){
             $("#" + hours[i]).addClass("grey");
+        }
+        else if(timeNow < parseInt(hours[i])){
+            $("#" + hours[i]).addClass("red");
         }
     }
 }
