e83ff7ca6079db9264877864c25f2ead728e3c8b
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-01-28 22:37:38 -0800

    consolidated js function to set local storage into a single function for each id

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,52 @@
+// Make sure document is ready for javascript to run //
 
+$(document).ready(function() {
 
-// Today's date, time during usage, and available times variables //
-var today = moment().format('MMMM Do YYYY');
-var timeNow = parseInt(moment().format('HH'));
-var hours = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17",];
+    // Today's date during usage //
+    var today = moment().format('MMMM Do YYYY');
+    // Add today's date to the Current Day display on html //
+     $("#currentDay").append(today);
+    // Today's time during usage //
+    var timeNow = parseInt(moment().format('HH'));
+   
 
-// Add today's date to the Current Day display on html //
-$("#currentDay").append(today);
+    // Options for times available  //
+    var hours = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17",];
 
-// Create loop to find past, current, and future time for styling //
-currentTime();
+    // Match times available to current time to set background color //
+    currentTime();
 
-function currentTime() {
+    function currentTime() {
 
-    for(var i = 0; i < hours.length; i++){
-        if (timeNow === parseInt(hours[i])){
-            $("#" + hours[i]).addClass("green");
-        }
-        else if (timeNow > parseInt(hours[i])){
-            $("#" + hours[i]).addClass("grey");
-        }
-        else if(timeNow < parseInt(hours[i])){
-            $("#" + hours[i]).addClass("red");
+        for(var i = 0; i < hours.length; i++){
+            if (timeNow === parseInt(hours[i])){
+                $("#" + hours[i]).addClass("green");
+            }
+            else if (timeNow > parseInt(hours[i])){
+                $("#" + hours[i]).addClass("grey");
+            }
+            else if(timeNow < parseInt(hours[i])){
+                $("#" + hours[i]).addClass("red");
+            }
         }
     }
-}
 
-let eightAM = $("#8");
-let nineAM = $("#9");
-let tenAM = $("#10");
-let elevenAM = $("#11");
-let twelvePM = $("#12");
-let onePM = $("#13");
-let twoPM = $("#14");
-let threePM = $("#15");
-let fourPM = $("#16");
-let fivePM = $("#17");
-
-
-// Create function to save text added into input to local storage //
-
-$("button").on("click",function() {
-    localStorage.setItem("8AMtext", (eightAM.val()))
-    localStorage.setItem("9AMtext", (nineAM.val()))
-    localStorage.setItem("10AMtext", (tenAM.val()))
-    localStorage.setItem("11AMtex", (elevenAM.val()))
-    localStorage.setItem("12PMtext", (twelvePM.val()))
-    localStorage.setItem("1PMtext", (onePM.val()))
-    localStorage.setItem("2PMtext", (twoPM.val()))
-    localStorage.setItem("3PMtext", (threePM.val()))
-    localStorage.setItem("4PMtext", (fourPM.val()))
-    localStorage.setItem("5PMtext", (fivePM.val()));
+    // Create function to save location and text of what was inputted //
+    $(".btn").on("click", function() {
+        var userTime = $(this).parent().attr("id");
+        var userInput = $("input").val();
+        localStorage.setItem(userTime, userInput);
+    });
+    // Attach the value (location, text) to the input //
+    $("#8am").children("input").val(localStorage.getItem("8am"));
+    $("#9am").children("input").val(localStorage.getItem("9am"));
+    $("#10am").children("input").val(localStorage.getItem("10am"));
+    $("#11am").children("input").val(localStorage.getItem("11am"));
+    $("#12pm").children("input").val(localStorage.getItem("12pm"));
+    $("#1pm").children("input").val(localStorage.getItem("1pm"));
+    $("#2pm").children("input").val(localStorage.getItem("2pm"));
+    $("#3pm").children("input").val(localStorage.getItem("3pm"));
+    $("#4pm").children("input").val(localStorage.getItem("4pm"));
+    $("#5pm").children("input").val(localStorage.getItem("5pm"));
 
 })
-
-// Now put that stored text onto the page //
-
-    $("#8").append(localStorage.getItem("8AMtext"));
-    $("#9").append(localStorage.getItem("9AMtext"));
-    $("#10").append(localStorage.getItem("10AMtext"));
-    $("#11").append(localStorage.getItem("11AMtext"));
-    $("#12").append(localStorage.getItem("12PMtext"));
-    $("#13").append(localStorage.getItem("1PMtext"));
-    $("#14").append(localStorage.getItem("2PMtext"));
-    $("#15").append(localStorage.getItem("3PMtext"));
-    $("#16").append(localStorage.getItem("4PMtext"));
-    $("#17").append(localStorage.getItem("5PMtext"));
-
